4feec1371f043655de271c288f0ba4b0ad431acc
MelinaRcldz <ricaldezmelina123@gmail.com>
2024-11-12 19:15:18 -0300

    cambios prueba

--- ordenes-compra/backend/server.js
+++ ordenes-compra/backend/server.js
@@ -1,8 +1,8 @@
 const dotenv = require('dotenv');
 dotenv.config();  // Para cargar variables de entorno desde .env
 
-const sequelize = require('./config/db');  // Conexion de Sequelize a la DB
-const app = require('./app');  // Crea una instancia de Express
+const sequelize = require('./config/db');  
+const app = require('./app');  
 
 // Importa los modelos
 const Producto = require('./models/Producto');
@@ -12,19 +12,17 @@ const Orden = require('./models/Orden');
 const Usuario = require('./models/Usuario');
 
 
-// Aquí se definen las relaciones entre los modelos
 Producto.hasMany(CarritoItem, { foreignKey: 'producto_id' });
 CarritoItem.belongsTo(Producto, { foreignKey: 'producto_id' });
 
 Carrito.hasMany(CarritoItem, { foreignKey: 'carrito_id' });
 CarritoItem.belongsTo(Carrito, { foreignKey: 'carrito_id' });
 
-// Sincroniza los modelos con la base de datos
-sequelize.sync()  // Si deseas reiniciar las tablas, usa `force: true`
+
+sequelize.sync()  
     .then(() => {
         console.log("Models sincronizados con la base de datos");
 
-        // Inicia el servidor Express
         const PORT = process.env.PORT || 3000;
         app.listen(PORT, () => {
             console.log(`Servidor corriendo en http://localhost:${PORT}`);
